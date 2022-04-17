b903e73204446b9608235f2d96a66b54b895f428
JiayuanWen <jbw5872@psu.edu>
2022-04-17 01:33:58 -0400

    Provided test server; Implemented basic backend functions.

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
+const express = require("express")
+const app = express()
 
+var port = 3000;
+
+
+
+app.listen((port), () => {
+    console.log("Server running on URL: https://localhost:"${port});
+})
