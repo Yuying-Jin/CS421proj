ed4728c2344c5f87c4369ca9e0190e3a1b3bb9bb
JiayuanWen <jbw5872@psu.edu>
2022-04-17 20:30:08 -0400

    Implemented Backend

--- testServer.js
+++ testServer.js
@@ -1,14 +1,24 @@
 // Importing the http module
 const http = require("http")
 
+// Determine port and host
+const hostname = '127.0.0.1'
+const port = 3000;
+
 // Creating server
 const server = http.createServer((req, res) => {
+
+    // Get Data from Client
+    req.on('data', chunk => {console.log("Got some data -->"+chunk)});
+    res.statusCode = 200;
+
     // Sending the response
-    res.write("This is the response from the server")
+    res.write("Respond from server.")
     res.end();
+
 })
 
 // Server listening to port 3000
-server.listen((3000), () => {
-    console.log("Server is Running");
+server.listen(port,hostname,()=> {
+    console.log("Server running");
 })
